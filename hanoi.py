ea6339fed9bed7e107ab898cd00bad0bcd7f14ba
Ke Wang <453587854@qq.com>
2018-01-06 12:19:30 +0800

    1st working version

--- hanoi.py
+++ hanoi.py
@@ -44,7 +44,32 @@ def train():
         current_state = best_next_state
 
 
-for i in range(0, 1000):
+for i in range(0, 10000):
     train()
 
-print reward_matrix
+
+def print_state(state):
+    pos_s = state % 3
+    pos_m = (state / 3) % 3
+    pos_l = (state / 9) % 3
+    print "s:" + str(pos_s) + " m:" + str(pos_m) + " l:" + str(pos_l)
+
+
+def print_solution():
+    current_state = 0
+
+    while current_state != final_state:
+        print_state(current_state)
+        possible_actions = reward_matrix[current_state]
+        best_next_state_quality = -10000
+        best_next_state = -1
+        for i in range(0, len(possible_actions)):
+            if possible_actions[i] > best_next_state_quality:
+                best_next_state_quality = possible_actions[i]
+                best_next_state = i
+        current_state = best_next_state
+
+    print_state(current_state)
+
+
+print_solution()
